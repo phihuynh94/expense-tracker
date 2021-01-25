a740d7ddc3d983fc1fc80aafdd405c4316ca6a97
phihuynh94 <phihuynh94@gmail.com>
2021-01-25 15:22:51 -0700

    prepared to deploy

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const path = require("path");
+
+dotenv.config({ path: "./config/config.env" });
+
+connectDB();
+
+const transactions = require("./routes/transactions");
+
+const app = express();
+app.use(express.json());
+
+app.use("/api/v1/transactions", transactions);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("/client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server running in ${PORT}`));
